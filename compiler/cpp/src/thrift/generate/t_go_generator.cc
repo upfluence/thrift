d1175169a9cd1a4358e996554749b8aea3555a16
Alexis Montagne <alexis.montagne@gmail.com>
2017-08-15 11:51:21 -0400

    compiler/go: Add Setter to the structs

--- compiler/cpp/src/thrift/generate/t_go_generator.cc
+++ compiler/cpp/src/thrift/generate/t_go_generator.cc
@@ -1408,12 +1408,24 @@ void t_go_generator::generate_go_struct_definition(ostream& out,
       out << indent() << "    return " << def_var_name << endl;
       out << indent() << "  }" << endl;
       out << indent() << "return " << maybepointer << "p." << publicized_name << endl;
+      out << indent() << "}" << endl << endl;
+
+      out << indent() << "func (p *" << tstruct_name << ") Set" << publicized_name << "(v "
+          << goType << ") {" << endl;
+
+      out << indent() << "  p." << publicized_name << " = &v" << endl;
       out << indent() << "}" << endl;
     } else {
       out << endl;
       out << indent() << "func (p *" << tstruct_name << ") Get" << publicized_name << "() "
           << goType << " {" << endl;
       out << indent() << "  return p." << publicized_name << endl;
+      out << indent() << "}" << endl << endl;
+
+      out << indent() << "func (p *" << tstruct_name << ") Set" << publicized_name << "(v "
+          << goType << ") {" << endl;
+
+      out << indent() << "  p." << publicized_name << " = v" << endl;
       out << indent() << "}" << endl;
     }
   }
