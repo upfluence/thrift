2e9ec01399c93fc631f939a792bf4e3a61cfb080
Alexis Montagne <alexis.montagne@gmail.com>
2017-12-27 14:54:08 +0100

    lib/rb/lib/thrift/processor: Handle exception in the processor and forward them as Thrift::ApplicationException:

--- lib/rb/lib/thrift/processor.rb
+++ lib/rb/lib/thrift/processor.rb
@@ -6,16 +6,16 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License. You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# 
+#
 
 module Thrift
   module Processor
@@ -24,18 +24,27 @@ module Thrift
     end
 
     def process(iprot, oprot)
-      name, type, seqid  = iprot.read_message_begin
+      name, _type, seqid = iprot.read_message_begin
       if respond_to?("process_#{name}")
-        send("process_#{name}", seqid, iprot, oprot)
+        begin
+          send("process_#{name}", seqid, iprot, oprot)
+        rescue => e
+          write_exception(e, oprot, name, seqid)
+        end
+
         true
       else
         iprot.skip(Types::STRUCT)
         iprot.read_message_end
-        x = ApplicationException.new(ApplicationException::UNKNOWN_METHOD, 'Unknown function '+name)
-        oprot.write_message_begin(name, MessageTypes::EXCEPTION, seqid)
-        x.write(oprot)
-        oprot.write_message_end
-        oprot.trans.flush
+        write_exception(
+          ApplicationException.new(
+            ApplicationException::UNKNOWN_METHOD,
+            'Unknown function '+name,
+          ),
+          oprot,
+          name,
+          seqid
+        )
         false
       end
     end
@@ -45,6 +54,21 @@ module Thrift
       args.read(iprot)
       iprot.read_message_end
       args
+    end
+
+    def write_exception(exception, oprot, name, seqid)
+      oprot.write_message_begin(name, MessageTypes::EXCEPTION, seqid)
+
+      unless exception.is_a? ApplicationException
+        exception = ApplicationException.new(
+          ApplicationException::INTERNAL_ERROR,
+          "Internal error processing #{name}: #{exception.class}: #{exception}"
+        )
+      end
+
+      exception.write(oprot)
+      oprot.write_message_end
+      oprot.trans.flush
     end
 
     def write_result(result, oprot, name, seqid)
