c2724565072afe11a92560ed747f610879b24680
Alexis Montagne <alexis.montagne@gmail.com>
2018-01-29 14:59:55 -0500

    compiler/cpp/src/generate/t_go_generator:  Add Interface() method over union to fetch the entity

---
+++ compiler/cpp/CMakeFiles/libparse.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "thrifty.cc"
+  "thrifty.hh"
+  "thriftl.cc"
+  "thrifty.h"
+  "CMakeFiles/libparse.dir/thrifty.cc.o"
+  "CMakeFiles/libparse.dir/thriftl.cc.o"
+  "../../lib/liblibparse.pdb"
+  "../../lib/liblibparse.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/libparse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
