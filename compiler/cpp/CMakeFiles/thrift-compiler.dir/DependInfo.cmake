2c7efa70a4e8224485194ba1683b325c3f9fd103
Alexis Montagne <alexis.montagne@gmail.com>
2018-01-29 14:59:55 -0500

    compiler/cpp/src/generate/t_go_generator:  Add Interface() method over union to fetch the entity

---
+++ compiler/cpp/CMakeFiles/thrift-compiler.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/md5.c" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/md5.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "compiler/cpp"
+  "thrift"
+  "."
+  "compiler/cpp/src"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_as3_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_as3_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_c_glib_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_c_glib_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_cocoa_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_cocoa_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_cpp_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_cpp_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_csharp_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_csharp_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_d_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_d_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_delphi_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_delphi_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_erl_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_erl_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_go_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_go_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_haxe_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_haxe_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_hs_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_hs_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_html_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_html_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_java_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_java_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_javame_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_javame_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_js_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_js_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_json_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_json_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_lua_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_lua_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_ocaml_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_ocaml_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_perl_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_perl_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_php_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_php_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_py_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_py_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_rb_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_rb_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_st_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_st_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/generate/t_xsd_generator.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/generate/t_xsd_generator.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/main.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/main.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/parse/parse.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/parse/parse.cc.o"
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/src/parse/t_typedef.cc" "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/thrift-compiler.dir/src/parse/t_typedef.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "compiler/cpp"
+  "thrift"
+  "."
+  "compiler/cpp/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/alexis/code/src/github.com/upfluence/thrift/compiler/cpp/CMakeFiles/libparse.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
