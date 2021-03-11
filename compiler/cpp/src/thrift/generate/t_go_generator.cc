67c1d174faded5e1e28649f925f0c1aeb9a95501
Alexis Montagne <alexis.montagne@gmail.com>
2021-03-11 14:13:10 -0500

    Add THRIFT_TYPES_PREFIX

--- compiler/cpp/src/thrift/generate/t_go_generator.cc
+++ compiler/cpp/src/thrift/generate/t_go_generator.cc
@@ -61,7 +61,7 @@ static const string endl = "\n"; // avoid ostream << std::endl flushes
  */
 bool format_go_output(const string& file_path);
 
-const string DEFAULT_THRIFT_IMPORT = "github.com/apache/thrift/lib/go/thrift";
+const string default_thrift_import = "github.com/upfluence/thrift/lib/go/thrift";
 static std::string package_flag;
 
 /**
