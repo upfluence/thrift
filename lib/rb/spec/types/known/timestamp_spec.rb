1b696615747b06e5c880576ec1bf12dd9816c1da
Alexis Montagne <alexis.montagne@gmail.com>
2021-04-08 15:42:57 -0400

    types/known/timestamp: Add standard type

---
+++ lib/rb/spec/types/known/timestamp_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'thrift/types/known/timestamp'
+
+describe 'Thrift::Types::Known::Timestamp' do
+  context 'from_time' do
+    it do
+      t = Time.at(137, 5, :nsec)
+
+      tt = Thrift::Types::Known::Timestamp.from_time(t)
+
+      tt.seconds.should == 137
+      tt.nanos.should == 5
+
+      tt.to_time.should == t
+    end
+  end
+end
