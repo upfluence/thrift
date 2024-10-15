0aa8b464f4fd0f04787a1cbeb9ef947f223c4c2c
Alexis Montagne <alexis.montagne@gmail.com>
2024-10-15 14:00:07 -0700

    compiler/cpp/src/main: Accept THRIFT_TYPES_PATH env variable

--- compiler/cpp/src/thrift/parse/t_program.h
+++ compiler/cpp/src/thrift/parse/t_program.h
@@ -59,9 +59,9 @@
 class t_program : public t_doc {
 public:
   t_program(std::string path, std::string name)
-    : path_(path), name_(name), out_path_("./"), out_path_is_absolute_(false), scope_(new t_scope), recursive_(false) {}
+    : path_(path), name_(name), out_path_("./"), out_path_is_absolute_(false), std_path_(THRIFT_TYPES_PATH), scope_(new t_scope), recursive_(false) {}
 
-  t_program(std::string path) : path_(path), out_path_("./"), out_path_is_absolute_(false), recursive_(false) {
+  t_program(std::string path) : path_(path), out_path_("./"), out_path_is_absolute_(false), std_path_(THRIFT_TYPES_PATH), recursive_(false)  {
     name_ = program_name(path);
     scope_ = new t_scope();
   }
@@ -74,7 +74,7 @@ public:
   }
 
   bool is_std_path() const {
-    return path_.rfind(THRIFT_TYPES_NESTED_PATH, 0) == 0;
+    return path_.rfind(std_path_ + "/types", 0) == 0;
   }
 
   // Path accessor
@@ -125,6 +125,8 @@ public:
   std::vector<t_program*>& get_includes() { return includes_; }
 
   const std::vector<t_program*>& get_includes() const { return includes_; }
+
+  void set_std_path(std::string std_path) { std_path_ = std_path; }
 
   void set_out_path(std::string out_path, bool out_path_is_absolute) {
     out_path_ = out_path;
@@ -398,6 +400,8 @@ private:
 
   // Include prefix for this program, if any
   std::string include_prefix_;
+
+  std::string std_path_;
 
   // Identifier lookup scope
   t_scope* scope_;
