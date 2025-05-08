285d03d9736c495a0af5cf37ac02fac7d02afe6a
Alexis Montagne <alexis.montagne@gmail.com>
2025-05-08 14:08:03 -0700

    lib/go/thrift: Improve the BufferTransport handling of context

--- lib/go/thrift/transport.go
+++ lib/go/thrift/transport.go
@@ -54,10 +54,6 @@ type TTransport interface {
 	WriteContext(ctx Context) error
 }
 
-type stringWriter interface {
-	WriteString(s string) (n int, err error)
-}
-
 // This is "enchanced" transport with extra capabilities. You need to use one of these
 // to construct protocol.
 // Notably, TSocket does not implement this interface, and it is always a mistake to use
@@ -66,7 +62,6 @@ type TRichTransport interface {
 	io.ReadWriter
 	io.ByteReader
 	io.ByteWriter
-	stringWriter
-	ContextFlusher
-	ReadSizeProvider
+	io.StringWriter
+	Flusher
 }
